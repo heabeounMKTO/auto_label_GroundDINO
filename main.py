ae300fac15cedaf09f2182dd50ec70699eec12d2
heabeounMKTO <heabeoun69zin@gmail.com>
2023-10-18 15:58:47 +0700

    tqdm

--- main.py
+++ main.py
@@ -3,18 +3,21 @@ from dino_finder import DinoFinder
 import os
 import cv2
 import json
+from tqdm import tqdm
 
 TEST_IMG = "test_set/t1.jpeg"
 cv_TEST_IMG = cv2.imread(TEST_IMG)
 CONFIG_PATH = "GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py" 
 WEIGHTS_PATH = os.path.join("weights", "groundingdino_swint_ogc.pth")
 finder = DinoFinder(CONFIG_PATH, WEIGHTS_PATH)
+workfolder = "/home/hbpopos/gtadset/sorted_dset"
 
-result_bbox, _conf, _cls = finder.find_by_prompt("human head. tree", TEST_IMG)
-print(result_bbox, _conf, _cls)
-
-labelz = Label(result_bbox, _cls, cv_TEST_IMG, TEST_IMG).create_label()
-jsonFilename = os.path.splitext(TEST_IMG)[0] + ".json"
-print(labelz)
-with open(jsonFilename, "w") as json_output:
-    json_output.write(json.dumps(labelz, indent=2))
+for image in tqdm(os.listdir(workfolder)):
+    if image.endswith(".jpeg"):
+        image_path = os.path.join(workfolder, image)
+        result_bbox, _conf, _cls = finder.find_by_prompt("human head", image_path)
+        labelz = Label(result_bbox, _cls, cv_TEST_IMG, image_path).create_label()
+        jsonFilename = os.path.splitext(image_path)[0] + ".json"
+        # print(labelz)
+        with open(jsonFilename, "w") as json_output:
+            json_output.write(json.dumps(labelz, indent=2))
